b256b8a021ff1e95d5720284fc6af1d23e299023
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-06-05 09:34:09 +0300

    Update run-finetuning-lumi-gpu1.sh

--- llm-inference/run-finetuning-lumi-gpu1.sh
+++ llm-inference/run-finetuning-lumi-gpu1.sh
@@ -13,7 +13,6 @@ module load lumi-aif-singularity-bindings
 
 export SIF=/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260415_130625/lumi-multitorch-full-u24r70f21m50t210-20260415_130625.sif
 
-
 # This will store all the Hugging Face cache such as downloaded models
 # and datasets in the project's scratch folder
 export HF_HOME=/scratch/${SLURM_JOB_ACCOUNT}/${USER}/hf-cache
@@ -27,14 +26,12 @@ mkdir -p $OUTPUT_DIR
 # want to retain direct control of parallelism options.
 export TOKENIZERS_PARALLELISM=false
 
-# Disable Weights & Biases logging
-export WANDB_DISABLED=true
-
 set -xv  # print the command so that we can verify setting arguments correctly from the logs
 
 srun singularity exec "$SIF" torchrun --standalone \
-     --nnodes=1 \
-     --nproc_per_node=1 \
-     finetuning.py $* \
-     --output-path $OUTPUT_DIR \
-     --num-workers $SLURM_CPUS_PER_TASK
+  --nnodes=1 \
+  --nproc_per_node=1 \
+  inference-lumi-demo.py \
+  --model openai/whisper-large-v3 \
+  --audio /scratch/project_462001302/mmahnoor/vllm/llm.inference/input/tests_data_multilingual.mp3
+
